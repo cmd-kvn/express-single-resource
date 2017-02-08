c8884d5726c0e08abe3e65a112137517b537ae3a
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-08 10:34:24 -0800

    finished test for double connecting to mongo and closing mongodb

--- test/connection.test.js
+++ test/connection.test.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const connection = require('../lib/connection.js');
+const connection = require('../lib/connection');
 
 // run "gomongo" in terminal before you run this test
 
@@ -10,12 +10,31 @@ describe('Connect to mongodb', () => {
     // connect to mongodb before the tests
     before(() => {
         return connection.connect(DB_URI)
-        // database from connection.connect is assigned to db
-        .then(_db => db = _db);
+            // database from connection.connect is assigned to db
+            .then(_db => db = _db);
     });
 
     it('assigned the return of .connect() to db', () => {
         assert.strictEqual(db, connection.db);
+    });
+
+    it('returns an error if trying to connect to a new db while connected', () => {
+        return connection.connect('mongodb://localhost:27017/anotherDb')
+            .then(
+            () => { throw new Error('should not resolve'); },
+            () => true
+            );
+            /*Question: why is there no assertion? */
+    });
+
+    it('resets connection.db to null on close', () => {
+        return connection.close()
+            .then(() => assert.isNull(connection.db));
+            /* Question: why does that^ work and not thisv? */
+            // .then(returnObj => {
+            //     console.log('returnobj is..', returnObj);
+            //     assert.isNull(returnObj.db)
+            // });
     });
 
 });
